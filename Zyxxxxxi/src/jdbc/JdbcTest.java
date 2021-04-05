282adf095c9d319fea0bd5d66db43641c7cf9041
zyxiiiii <1069227225@qq.com>
2021-04-05 10:52:30 +0800

    JDBC学习

--- Zyxxxxxi/src/jdbc/JdbcTest.java
+++ Zyxxxxxi/src/jdbc/JdbcTest.java
@@ -4,4 +4,9 @@ package jdbc;
  * @author zyx
  */
 public class JdbcTest {
+    public static void main(String[] args) {
+        JdbcDemo jdbc = new JdbcDemo();
+        jdbc.dmlExecutor("insert into tbl_test1(StudentName,ClassNumber) values('zyx',5),('lqq',5)");
+        jdbc.close();
+    }
 }
